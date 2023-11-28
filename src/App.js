e2e2d9b3dad2bf5290fbb0d1da41335e45a1503e
Himanshu <phimanshu925@gmail.com>
2023-11-28 23:27:34 +0530

    himanshufirstpatch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 import Form from './components/Form'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import HomeComponent from './components/HomeComponent';
+import {Routes,Route} from 'react-router-dom';
+import StudentDetails from './components/StudentDetails';
+import StudentList from './components/StudentList';
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"></link>
 function App() {
   return (
     <div className="App">
-     <Form></Form>
+     <h1 style={{"backgroundColor":"blue",color:"white","border":"2px solid red"}}>IACSD student managment</h1>
+      
+        {/* <MainNavBar></MainNavBar> */}
+       <Routes>
+            <Route path="/home" element={<HomeComponent></HomeComponent>}></Route>
+            <Route path="/table" element={<StudentDetails></StudentDetails>}></Route>
+            <Route path="/list" element={<StudentList></StudentList>}></Route>
+            {/* <Route path="/form" element={<From></From>}></Route> */}
+            {/* <Route path="/edit/:pid" element={<ProductEdit></ProductEdit>}></Route>
+            <Route path="/view/:pid" element={<StudentCard></StudentCard>}></Route> */}
+           
+       </Routes>
     </div>
   );
 }
--- src/components/StudentList.jsx
+++ src/components/StudentList.jsx
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import StudentService from '../services/StudentService';
+import StudentDetails from './StudentDetails'
+import {Link} from 'react-router-dom';
+export default function StudentList(props) {
+  
+          const [studentArr,setstudentArr]=useState([]);
 
-export default function StudentList() {
+          const fetchStudent=()=>{
+            StudentService.getAllStudent()
+            .then((result)=>{
+              console.log(result);
+              setstudentArr([...result.data]);
+            }).catch((err)=>{
+              console.log("error occured",err)
+            })
+          }
+          useEffect(()=>{
+            console.log("in List Initialization useEffect");
+            fetchStudent();
+          },[])
 
 
   return (
     <div>
-      <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-      </ul>
-    </div>
+    <Link to="/form">
+     <button type="button" name="btn" className="btn btn-primary" >Add new Product</button>&nbsp;&nbsp;&nbsp;
+     </Link>
+    {/* step-5 add productDetails component */}
+      {studentArr.map(s=><StudentDetails key={s.rollno}  getData={fetchStudent} student={s}></StudentDetails>)}
+     
+      </div>
   )
 }
